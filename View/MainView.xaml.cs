688e90e4f7b3d6fd4ba4445759b4ee817ebdedf0
GSS-DCG <darien.callejo@gsolarsteel.com>
2025-05-23 12:22:00 +0200

    V0.2

--- View/MainView.xaml.cs
+++ View/MainView.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using System.Windows.Interop;
+using SmarTools.ViewModel;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ModernUI.View
 {
@@ -21,10 +25,27 @@ namespace ModernUI.View
     /// </summary>
     public partial class MainView : Window
     {
+        public static class Globales
+        {
+            public static string _version = "Version_0.0";
+
+            public static Inicio _inicio = new Inicio();
+            public static TracSmart1V _TracSmart1V = new TracSmart1V();
+            public static TracSmart2V _TracSmart2V = new TracSmart2V();
+            public static RackSmart _RackSmart = new RackSmart();
+            public static Ajustes _Ajustes = new Ajustes();
+            public static FontAwesome.Sharp.IconImage _BellNotification = new FontAwesome.Sharp.IconImage();
+        }
         public MainView()
         {
             InitializeComponent();
+            Caption_Text.Text = "Inicio";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.Home;
+            MainViewContentControl.Children.Add(Globales._inicio);
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            Globales._BellNotification = BellNotification;
+            Herramientas.NotificacionCampana();
         }
 
         [DllImport("user32.dll")]
@@ -39,6 +60,102 @@ namespace ModernUI.View
         private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
         {
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal) { this.WindowState = WindowState.Maximized; } else { this.WindowState = WindowState.Normal; }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnSettingsIcon_Click(object sender, RoutedEventArgs e)
+        {
+            Caption_Text.Text = "Ajustes";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.Gear;
+            MainViewContentControl.Children.Clear();
+            MainViewContentControl.Children.Add(Globales._Ajustes);
+        }
+
+        private System.Drawing.Color backColor;
+        private System.Drawing.Color borderColor;
+        private System.Drawing.Color menuItemBorderColor;
+        private System.Drawing.Color menuItemSelectedColor;
+
+        private void btnBellIcon_Click(object sender, RoutedEventArgs e)
+        {
+            backColor = System.Drawing.Color.FromArgb(0,0,0);
+            backColor = System.Drawing.Color.FromArgb(255,0,0);
+            backColor = System.Drawing.Color.FromArgb(0,255,0);
+            backColor = System.Drawing.Color.FromArgb(0,0,255);
+
+            
+        }
+
+        private void btnInicio_Click(object sender, RoutedEventArgs e)
+        {
+            Caption_Text.Text = "Inicio";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.Home;
+            MainViewContentControl.Children.Clear();
+            MainViewContentControl.Children.Add(Globales._inicio);
+            SmarTools.ViewModel.Inicio.InfoVersion();
+        }
+
+        private void btnTracSmart1V_Click(object sender, RoutedEventArgs e)
+        {
+            Caption_Text.Text = "TracSmart+ 1P";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.SolarPanel;
+            MainViewContentControl.Children.Clear();
+            MainViewContentControl.Children.Add(Globales._TracSmart1V);
+        }
+
+        private void btnTracSmart2V_Click(object sender, RoutedEventArgs e)
+        {
+            Caption_Text.Text = "TracSmart+ 2P";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.SolarPanel;
+            MainViewContentControl.Children.Clear();
+            MainViewContentControl.Children.Add(Globales._TracSmart2V);
+        }
+
+        private void btnRacksmart_Click(object sender, RoutedEventArgs e)
+        {
+            Caption_Text.Text = "RackSmarT";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.SolarPanel;
+            MainViewContentControl.Children.Clear();
+            MainViewContentControl.Children.Add(Globales._RackSmart);
+        }
+
+        private void btnAjustes_Click(object sender, RoutedEventArgs e)
+        {
+            Caption_Text.Text = "Ajustes";
+            Caption_Icon.Icon = FontAwesome.Sharp.IconChar.Gear;
+            MainViewContentControl.Children.Clear();
+            MainViewContentControl.Children.Add(Globales._Ajustes);
+        }
+    }
+    public class Herramientas
+    {
+        public static void NotificacionCampana()
+        {
+            string ruta = @"Z:\300Logos\Version.txt";
+            string version = File.ReadAllText(ruta);
+
+            if (version != ModernUI.View.MainView.Globales._version)
+            {
+                ModernUI.View.MainView.Globales._BellNotification.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ModernUI.View.MainView.Globales._BellNotification.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
---
+++ ViewModel/TracSmart1V.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmarTools.ViewModel
+{
+    /// <summary>
+    /// Lógica de interacción para TracSmart1V.xaml
+    /// </summary>
+    public partial class TracSmart1V : UserControl
+    {
+        public TracSmart1V()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumeroPilares_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCambiarCombinaciones_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCambiarCargas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDimensionamiento_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnItalia_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCalcularFlechas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnComprobacionUniones_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReacciones_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnListadosCalculo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
